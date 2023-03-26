05926361b57c65b1edd8ecfc22879a31d93e368d
httpsrute <125400636+httpsrute@users.noreply.github.com>
2023-03-26 17:59:45 -0300

    Add files via upload

---
+++ Atividade3/PImc/PImc/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace PImc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.lblIMC = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtIMC = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPeso.Location = new System.Drawing.Point(17, 23);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(45, 20);
+            this.lblPeso.TabIndex = 0;
+            this.lblPeso.Text = "Peso";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltura.Location = new System.Drawing.Point(17, 59);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(51, 20);
+            this.lblAltura.TabIndex = 1;
+            this.lblAltura.Text = "Altura";
+            // 
+            // lblIMC
+            // 
+            this.lblIMC.AutoSize = true;
+            this.lblIMC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIMC.Location = new System.Drawing.Point(17, 89);
+            this.lblIMC.Name = "lblIMC";
+            this.lblIMC.Size = new System.Drawing.Size(38, 20);
+            this.lblIMC.TabIndex = 2;
+            this.lblIMC.Text = "IMC";
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(74, 23);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(313, 20);
+            this.txtPeso.TabIndex = 3;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(74, 59);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(313, 20);
+            this.txtAltura.TabIndex = 4;
+            this.txtAltura.Validated += new System.EventHandler(this.txtAltura_Validated);
+            // 
+            // txtIMC
+            // 
+            this.txtIMC.Enabled = false;
+            this.txtIMC.Location = new System.Drawing.Point(74, 91);
+            this.txtIMC.Name = "txtIMC";
+            this.txtIMC.Size = new System.Drawing.Size(313, 20);
+            this.txtIMC.TabIndex = 5;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(21, 130);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(240, 57);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(267, 130);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(120, 30);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(267, 166);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(120, 21);
+            this.btnFechar.TabIndex = 8;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 199);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtIMC);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.lblIMC);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblPeso);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Label lblIMC;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtIMC;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnFechar;
+    }
+}
+
---
+++ Atividade3/PImc/PImc/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PImc
+{
+    public partial class Form1 : Form
+    {
+        double altura, peso, IMC;
+
+        private void txtPeso_Validated(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtPeso.Text, out peso))
+            {
+                MessageBox.Show("Peso inválido!");
+                txtPeso.Focus();
+            }
+        }
+
+        private void txtAltura_Validated(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtAltura.Text, out altura))
+            {
+                MessageBox.Show("Altura inválida!");
+                txtAltura.Focus();
+            }
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if ((double.TryParse(txtAltura.Text, out altura) &&
+                 double.TryParse(txtPeso.Text, out peso)))
+
+            {
+                if ((peso <= 0) || (altura <= 0))
+                    MessageBox.Show("Os valores devem ser maiores que ZERO (0)!");
+
+                else
+                {
+                    peso = peso * 100;
+                    IMC = (peso / Math.Pow(altura, 2)) / 100;
+                    IMC = Math.Round(IMC, 1);
+                    txtIMC.Text = IMC.ToString("N1");
+
+                    if (IMC < 18.5)
+                        MessageBox.Show("Classificação: magreza. \r\nGrau de obesidade: 0.");
+                    else if (IMC <= 24.9)
+                        MessageBox.Show("Classificação: normal. \r\nGrau de obesidade: 0.");
+                    else if (IMC <= 29.9)
+                        MessageBox.Show("Classificação: sobrepeso. \r\nGrau de obesidade: I.");
+                    else if (IMC <= 39.9)
+                        MessageBox.Show("Classificação: obesidade. \r\nGrau de obesidade: II.");
+                    else
+                        MessageBox.Show("Classificação: obesidade grave. \r\nGrau de obesidade: III.");
+                }
+            }
+            else
+                MessageBox.Show("Valores inválidos.");
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtPeso.Clear();
+            txtAltura.Clear();
+            txtIMC.Clear();
+
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
